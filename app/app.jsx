b2e7a822d16a957e6515d7c4609592ce67e5ad6f
endo <env.endo@gmail.com>
2016-12-29 17:25:01 +0900

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      Boiler Plate
+    </div>
+  )
+}
+
+ReactDom.render(
+  <App />,
+  document.getElementById('app')
+)
